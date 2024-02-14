7c4b9e47da6166f056769e0ae03d5d51ab3dd7cb
Mariana <75488872+marianapequeno@users.noreply.github.com>
2024-02-14 16:45:01 -0300

    first commit - environment

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from "@angular/core";
+
+const routes : Routes = [
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule, //Para animações no navegador
+    AppRoutingModule,
+    HttpClientModule, //Para fazer requisições HTTP (Consumir APIs)
+    FormsModule, //Para fazer formulários simples (FormsModule)
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
